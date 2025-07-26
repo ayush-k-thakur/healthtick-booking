7baca5926c1d7d21b39040c904cb1d7619d034fa
Ayush Kumar Thakur <ayush524425@gmail.com>
2025-07-26 19:25:42 +0545

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { collection, addDoc } from "firebase/firestore";
-import db from "./firebase";
 import CalenderSchedule from "./components/CalenderSchedule";
 
 export default function App() {
-
-  // const writeData = async () => {
-  //   try {
-  //     const docRef = await addDoc(collection(db, "appointments"), {
-  //       name: "John Doe",
-  //       time: "10:30 AM",
-  //       status: "Available"
-  //     });
-  //     console.log("Document written with ID: ", docRef.id);
-  //   } catch (e) {
-  //     console.error("Error adding document: ", e);
-  //   }
-  // };
-
-  return (
+   return (
     <>
       <CalenderSchedule />
-      {/* <div>
-        <button onClick={writeData}>Firestore</button>
-      </div> */}
     </>
   );
 }
